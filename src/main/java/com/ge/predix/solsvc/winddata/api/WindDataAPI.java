e81613180c7b3c3a8771957e1cf2c968e0f0d6c7
desumitrais <made.sudarsana@mitrais.com>
2017-05-24 09:15:51 +0800

    initialization

---
+++ src/main/java/com/ge/predix/solsvc/winddata/api/WindDataAPI.java
@@ -0,0 +1,84 @@
+package com.ge.predix.solsvc.winddata.api;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import com.ge.predix.entity.timeseries.datapoints.ingestionrequest.Body;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * 
+ * @author predix -
+ */
+@Consumes(
+{
+        "application/json", "application/xml"
+})
+@Produces(
+{
+        "application/json", "application/xml"
+})
+@Path("/windservices")
+@Api(value = "/windservices")
+public interface WindDataAPI
+{
+    /**
+     * @return -
+     */
+    @GET
+    @Path("/ping")
+    @ApiOperation(value = "/ping")
+    public Response greetings();
+
+
+    /**
+     * @param id
+     *            -
+     * @param authorization
+     *            -
+     * @param starttime
+     *            -
+     * @param tagLimit -
+     * @param tagorder -
+     * @return -
+     */
+    @GET
+    @Path("/yearly_data/sensor_id/{id}")
+    @ApiOperation(value = "/yearly_data/sensor_id/{id}")
+    public Response getYearlyWindDataPoints(@PathParam("id") String id,
+            @HeaderParam(value = "Authorization") String authorization,
+            @DefaultValue("1y-ago") @QueryParam("starttime") String starttime,
+            @DefaultValue("25") @QueryParam("taglimit") String tagLimit,
+            @DefaultValue("desc") @QueryParam("order") String tagorder);
+
+    /**
+     * @param id
+     *            -
+     * @param authorization
+     *            -
+     * @return -
+     */
+    @GET
+    @Path("/latest_data/sensor_id/{id}")
+    @ApiOperation(value = "/latest_data/sensor_id/{id}")
+    public Response getLatestWindDataPoints(@PathParam("id") String id,
+            @HeaderParam(value = "authorization") String authorization);
+
+    /**
+     * 
+     * @param authorization -
+     * @return List of tags
+     */
+    @GET
+    @Path("/tags")
+    @ApiOperation(value ="/tags")
+    public Response getWindDataTags(@HeaderParam(value = "authorization") String authorization);
+
+    @POST
+    @Path("/temperature")
+    @ApiOperation(value ="/temperature")
+    public Response addTempratureDataTags(@RequestBody Body bodyMessage);
+
+}
